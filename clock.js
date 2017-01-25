378f3da6dec81a2d8167bb47942c10f410674a03
andre the <andre.the12@gmail.com>
2017-01-25 17:35:22 +0800

    added namespaces

--- clock.js
+++ clock.js
@@ -1,25 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
+function overCode () {
   var seconds = document.querySelector('#second')
   var minute = document.querySelector('#minute')
   var hour = document.querySelector('#hour')
   var clock = document.querySelector('#clock')
   // var degrees = 45
-  clock.appendChild(minute)
-  clock.appendChild(seconds)
-
-
-
 
   // var count = 0
 
   function secRotation (secTime) {
-    return (secTime /60 ) * 360
+    return (secTime / 60) * 360
   }
   function hrRotation (hourTime) {
     return (hourTime / 12) * 360
   }
   function minRotation (minTime) {
-    return (minTime /60 ) * 360
+    return (minTime / 60) * 360
   }
 
   // Make second move first
@@ -32,13 +27,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function setRotation () {
     var now = new Date()
-      var currentHour = now.getHours()
-      var currentMinute = now.getMinutes()
-      var currentSecond = now.getSeconds()
+    var currentHour = now.getHours()
+    var currentMinute = now.getMinutes()
+    var currentSecond = now.getSeconds()
 
     var minRota = minRotation(currentMinute) // divide by 60 to get secs
     var secRota = secRotation(currentSecond)
-    var hourRota = hrRotation(currentHour) //divide by 3600 to get hours
+    var hourRota = hrRotation(currentHour) // divide by 3600 to get hours
 
     // var handsArr = [seconds, minute, hour]
     // var rotaArr = [secRota, minRota, hourRota]
@@ -50,13 +45,21 @@ document.addEventListener('DOMContentLoaded', function () {
     minute.style.transform = 'rotate(' + minRota + 'deg)'
     hour.style.transform = 'rotate(' + hourRota + 'deg)'
     // count++
+    }
+    return{
+      setRotation: setRotation,
+      secRotation: secRotation,
+      hrRotation: hrRotation,
+      minRotation: minRotation
   }
-
+}
 
   // secondRotation(0)
   // secondRotation(59)
   //
   // hourRotation(12)
   // hourRotation(5)
-  setInterval(setRotation, 1000)
+window.addEventListener('DOMContentLoaded', function () {
+var domClock =overCode()
+setInterval(domClock.setRotation, 1000)
 })
